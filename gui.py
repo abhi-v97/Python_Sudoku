aa5a51977084d14df289e0055a18be9f25d6ab36
abhiv-97 <v.abhishek@outlook.com>
2021-11-14 23:00:12 +0100

    sudoku grid done

--- gui.py
+++ gui.py
@@ -1,10 +1,122 @@
 # GUI.py
 import pygame
 from sudoku_solver import solve, valid
-import sudoku_generator
+from sudoku_generator import fill, erase
 import time
 pygame.font.init()
 
+
+board1 = [
+    [8, 0, 5, 0, 0, 0, 0, 0, 0],
+    [0, 4, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 3, 0, 6, 0, 8],
+    [0, 0, 9, 8, 0, 1, 0, 0, 6],
+    [4, 1, 0, 2, 0, 0, 0, 0, 0],
+    [0, 0, 8, 0, 0, 0, 1, 0, 0],
+    [1, 0, 0, 0, 4, 0, 3, 9, 5],
+    [5, 8, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 4, 0, 0, 7, 0, 0, 0]
+]
+
+class Grid:
+    board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+    #board = board1
+
+    # Create a fully solved sudoku board, then erase values at random to create the challenge 
+    # using methods from the sudoku generator
+    fill(board)
+    erase(board)
+
+    def __init__(self, rows, cols, width, height):
+        self.rows = rows
+        self.cols = cols
+        self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
+        self.width = width
+        self.height = height
+        self.model = None
+        self.selected = None
+
+    # Check if inputted number is a legal move, and if it can lead to a valid solution
+    def update_model(self):
+        self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
+
+    def place(self, val):
+        row, col = self.selected
+        if self.cubes[row][col].value == 0:
+            self.cubes[row][col].set(val)
+            self.update_model()
+
+            if valid(self.model, val, row,col) and solve(self.model):
+                return True
+            else:
+                self.cubes[row][col].set(0)
+                self.cubes[row][col].set_temp(0)
+                self.update_model()
+                return False
+
+    def sketch(self, val):
+        row, col = self.selected
+        self.cubes[row][col].set_temp(val)
+
+    def draw(self, win):
+        # Draw grud lines
+        gap = self.width / 9
+        for i in range(self.rows+1):
+            if i % 3 == 0 and i != 0:
+                thick = 4
+            else:
+                thick = 1
+            pygame.draw.line(win, (0,0,0), (0, i*gap), (self.width, i*gap), thick)
+            pygame.draw.line(win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
+
+        # Place cubes in 9x9 board
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].draw(win)
+
+    def select(self, row, col):
+        # Highlight the clicked cube
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].selected = False
+
+        self.cubes[row][col].selected = True
+        self.selected = (row, col)
+
+    def clear(self):
+        row, col = self.selected
+        if self.cubes[row][col].value == 0:
+            self.cubes[row][col].set_temp(0)
+
+    def click(self, pos):
+        """
+        :param: pos
+        :return: (row, col)
+        """
+        if pos[0] < self.width and pos[1] < self.height:
+            gap = self.width / 9
+            x = pos[0] // gap
+            y = pos[1] // gap
+            return (int(y),int(x))
+        else:
+            return None
+
+    def is_finished(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.cubes[i][j].value == 0:
+                    return False
+        return True
 
 # The sudoku board is made up of 9x9 cubes, this class defines the cubes
 class Cube:
